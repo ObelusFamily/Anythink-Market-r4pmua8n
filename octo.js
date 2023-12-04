f0df56dfb3f17fafc19e321fc9f7a145408ff527
Evan Schirle <eschirle13@gmail.com>
2023-12-04 22:29:44 +0000

    get repo

--- octo.js
+++ octo.js
@@ -1,3 +1,10 @@
 const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({auth: "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"});
+
+const repos = octokit.rest.repos.get({
+    owner: "United-Airlines-Org",
+    repo: "cjq.infra"
+});
+
+console.log(repos);
